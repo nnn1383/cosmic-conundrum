764bb2434d68592e83336af405848605bd385109
Nishi <93357010+nnn1383@users.noreply.github.com>
2025-05-07 19:16:38 -0400

    kinda?

---
+++ src/Question.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+
+function Question() {
+    return (
+        <>
+            <input type="button" value="Start Adventure" className="start-button"></input>
+        </>
+    )
+}
+
+export default Question;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Question from "./Question";
+import './styles.css';
+
+function Page() {
+    return (
+        <div className="homepage">
+            <main>
+                <h1 className="adventure-title">Cosmics Conundrum</h1>
+            </main>
+            <img src="src/images/spaceshipgif.gif"></img>
+            <Question />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
